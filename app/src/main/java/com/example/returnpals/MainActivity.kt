3b9c37e8024573803b2f05ec822bc17910ba9346
Gustavo-Gon <gon.gustavo@outlook.com>
2023-11-22 14:40:18 -0800

    More Figma Relay changes

--- app/src/main/java/com/example/returnpals/MainActivity.kt
+++ app/src/main/java/com/example/returnpals/MainActivity.kt
@@ -5,11 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.returnpals.ui.theme.Background
 import com.example.returnpals.ui.theme.ReturnPalsTheme
 
@@ -23,20 +19,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = Background
                 ) {
-                    AppNavigation()
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun AppNavigation() {
-    val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "splash") {
-        composable("splash") {
-            SplashScreen(onSplashEnd = { navController.navigate("login") })
-        }
-        composable("login") { LoginScreenComposable() }
     }
 }
