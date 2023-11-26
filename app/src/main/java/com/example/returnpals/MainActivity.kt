dfa3c23635845bf431977dcd30a2eb4ebfd1b4f0
David Caldwell <david.caldwell@bellevuecollege.edu>
2023-11-26 13:50:11 -0800

    Ran thankyou in main activity

--- app/src/main/java/com/example/returnpals/MainActivity.kt
+++ app/src/main/java/com/example/returnpals/MainActivity.kt
@@ -11,36 +11,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.returnpals.ui.theme.ReturnPalsTheme
+import com.example.returnpals.thankyou.ThankYou
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ReturnPalsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            ThankYou()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ReturnPalsTheme {
-        Greeting("Android")
-    }
-}
