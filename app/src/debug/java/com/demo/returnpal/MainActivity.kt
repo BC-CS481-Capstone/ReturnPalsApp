1413e825f8f4ccc0c0be8cda6da5c7e45cbf4047
David Caldwell <david.caldwell@bellevuecollege.edu>
2023-11-26 22:03:19 -0800

    Connected two activities

--- app/src/debug/java/com/demo/returnpal/MainActivity.kt
+++ app/src/debug/java/com/demo/returnpal/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.demo.returnpal
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.demo.returnpal.thankyou.ThankYou
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.demo.returnpal.confirmpickup.ConfirmPickup
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           ThankYou()
+            ConfirmPickup()
+            createbuttonConfirm()
+        }
+    }
+    @Composable
+    fun createbuttonConfirm() {
+        Button(onClick = {
+            var x = Intent(this,ThankYouActivity::class.java)
+            startActivity(x)
+        }) {
+            Text("HERE PUSH ME")
         }
     }
 }
+
