7694b2d73b700fae8d049b71e09b0e485e302713
David Caldwell <david.caldwell@bellevuecollege.edu>
2023-11-26 17:48:22 -0800

    replaced example package with demo

--- app/src/debug/java/com/demo/returnpal/MainActivity.kt
+++ app/src/debug/java/com/demo/returnpal/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.returnpals
+package com.demo.returnpal
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,13 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.returnpals.ui.theme.ReturnPalsTheme
+import com.demo.returnpal.ui.theme.ReturnPalsTheme
 import com.example.returnpals.thankyou.ThankYou
-class ThankYou : ComponentActivity() {
+
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ThankYou()
+           ThankYou()
         }
     }
 }
