1f293dd4251a701bac9f6e8ea455ac8cc0360435
David Caldwell <david.caldwell@bellevuecollege.edu>
2023-11-26 19:38:29 -0800

    removed old, corrected new build files

--- app/src/debug/java/com/demo/returnpal/MainActivity.kt
+++ app/src/debug/java/com/demo/returnpal/MainActivity.kt
@@ -3,15 +3,7 @@ package com.demo.returnpal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.demo.returnpal.ui.theme.ReturnPalsTheme
-import com.example.returnpals.thankyou.ThankYou
+import com.demo.returnpal.thankyou.ThankYou
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
