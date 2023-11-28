e0fac3c22b2647f6f133600339a948ff65d8923b
David Caldwell <david.caldwell@bellevuecollege.edu>
2023-11-27 19:05:50 -0800

    PackageInfoShowLabelActivity set to next activity

--- app/src/debug/java/com/demo/returnpal/PackageInfoAddLabelActivity.kt
+++ app/src/debug/java/com/demo/returnpal/PackageInfoAddLabelActivity.kt
@@ -1,29 +1,26 @@
 package com.demo.returnpal
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.demo.returnpal.ui.theme.ReturnPalsAppTheme
+import com.demo.returnpal.packageinfoaddlabelcomponent.PackageInfoAddLabelComponent
 
 class PackageInfoAddLabelActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PackageInfoComponent()
+            PackageInfoAddLabelComponent()
             createbuttonConfirm()
         }
     }
     @Composable
     fun createbuttonConfirm() {
         Button(onClick = {
-            var x = Intent(this,ThankYouActivity::class.java)
+            var x = Intent(this,PackageInfoShowLabelActivity::class.java)
             startActivity(x)
         }) {
             Text("HERE PUSH ME")
