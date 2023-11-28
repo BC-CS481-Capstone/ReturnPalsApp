51dcb69bb1b2f7c9ed007bc8081cc30c64a2a9d4
Andrew Pogue <andrewthomaspogue@gmail.com>
2023-11-27 16:24:23 -0800

    imported choose plan screen from figma

--- app/src/main/java/com/example/returnpals/ChoosePlanActivity.kt
+++ app/src/main/java/com/example/returnpals/ChoosePlanActivity.kt
@@ -1,18 +1,17 @@
 package com.example.returnpals
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.returnpals.ui.theme.ReturnPalsTheme
 
-class MainActivity : ComponentActivity() {
+class ChoosePlanActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +21,16 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ChoosePlan()
+                    ChoosePlan(
+                        modifier = Modifier,
+                        onBackButtonTapped = {},
+                        onSilverPlanButtonTapped = {},
+                        onPlatinumPlanButtonTapped = {},
+                        onGoldPlanButtonTapped = {},
+                        onBronzePlanButtonTapped = {},
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ReturnPalsTheme {
-        Greeting("Android")
     }
 }
