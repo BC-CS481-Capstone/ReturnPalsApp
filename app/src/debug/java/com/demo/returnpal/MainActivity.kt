edaa84bf8cdd950b3dc0bc264777d65412fe48f6
David Caldwell <david.caldwell@bellevuecollege.edu>
2023-11-28 17:17:27 -0800

    create flow start

--- app/src/debug/java/com/demo/returnpal/MainActivity.kt
+++ app/src/debug/java/com/demo/returnpal/MainActivity.kt
@@ -4,20 +4,39 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.demo.returnpal.packageinfocomponent.PackageInfoComponent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //If Guest
-            // startActivity(Intent(this,PackageInfoGuestActivity::class.java))
-            //Else user
-            startActivity(Intent(this,PackageInfoActivity::class.java))
+            buttonChoice(guest = {startActivity(Intent(this,PackageInfoGuestActivity::class.java))},
+                user = {startActivity(Intent(this,PackageInfoActivity::class.java))})
         }
     }
 }
+@Composable
+fun buttonChoice(guest: () -> Unit = {},
+                 user: () -> Unit = {}) {
+    Row(){
+        guestButton(guest)
+        userButton(user)
+    }
+}
 
+@Composable
+fun guestButton(onClick: () -> Unit) {
+    Button(onClick = {onClick()}) {
+       Text("Guest")
+    }
+}
+
+@Composable
+fun userButton(onClick: () -> Unit) {
+    Button(onClick = {onClick()}) {
+        Text("User")
+    }
+}
