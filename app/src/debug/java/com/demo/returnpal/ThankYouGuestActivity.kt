09327c5bae96a7839b5011e721cd22888b55b888
David Caldwell <david.caldwell@bellevuecollege.edu>
2023-11-28 17:20:16 -0800

    ThankYouGuestActivity changed to ThankYouGuestComponent

--- app/src/debug/java/com/demo/returnpal/ThankYouGuestActivity.kt
+++ app/src/debug/java/com/demo/returnpal/ThankYouGuestActivity.kt
@@ -3,12 +3,13 @@ package com.demo.returnpal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.demo.returnpal.thankyouguestcomponent.ThankYouGuestComponent
 
 class ThankYouGuestActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ThankYouGuestActivity()
+            ThankYouGuestComponent()
         }
     }
 }
