f95873908c922b6aadcbbd9af1abf9dd1c66fb43
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-01-12 21:07:45 -0800

    call splash screen function

--- app/src/main/java/com/example/returnpals/MainActivity.kt
+++ app/src/main/java/com/example/returnpals/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.returnpals
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.returnpals.composetools.NextButton
 
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             NextButton(onClick = {})
         }
