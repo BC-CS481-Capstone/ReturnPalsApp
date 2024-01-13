ad40218546c7770a477bee160c23076e1f76a415
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-01-12 20:39:19 -0800

    added packages composetools and services

--- app/src/main/java/com/example/returnpals/MainActivity.kt
+++ app/src/main/java/com/example/returnpals/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.returnpals
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.returnpals.composetools.NextButton
 
 
 class MainActivity : ComponentActivity() {
