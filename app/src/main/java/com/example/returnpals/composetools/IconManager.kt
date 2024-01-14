2ea142e3e7f5d676e99539dbc7df8c5af8b8efe5
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-01-13 20:18:49 -0800

    add IconManager class

---
+++ app/src/main/java/com/example/returnpals/composetools/IconManager.kt
@@ -0,0 +1,13 @@
+package com.example.returnpals.composetools
+
+class IconManager {
+    fun getTruckIcon() {}
+    fun getPriceIcon() {}
+    fun getComputerMonitorIcon() {}
+    fun getQRcodeIcon() {}
+    fun getAmazonIcon() {}
+    fun getFileIcon() {}
+    fun getDoorIcon() {}
+    fun getPackageIcon() {}
+
+}
