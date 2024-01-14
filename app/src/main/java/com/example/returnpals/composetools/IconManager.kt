bc68425a937e7302c8e2e50ec907704906c8fde1
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-01-13 20:24:49 -0800

    add description

--- app/src/main/java/com/example/returnpals/composetools/IconManager.kt
+++ app/src/main/java/com/example/returnpals/composetools/IconManager.kt
@@ -1,6 +1,11 @@
 package com.example.returnpals.composetools
 
 class IconManager {
+    /*
+    This is the icon manager class. Use this class to call Icons from resource folder.
+    Uses the painter object api to pull R.drawable resources into an Icon object.
+    Folloed examples from https://developer.android.com/jetpack/compose/resources
+    * */
     fun getTruckIcon() {}
     fun getPriceIcon() {}
     fun getComputerMonitorIcon() {}
