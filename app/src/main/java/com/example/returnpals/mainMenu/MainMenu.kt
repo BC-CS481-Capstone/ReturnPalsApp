7ff72e66463432a0283b0cd3300d408cb55a9dac
Gustavo-Gon <gon.gustavo@outlook.com>
2024-01-17 21:10:58 -0800

    FAQ added

--- app/src/main/java/com/example/returnpals/mainMenu/MainMenu.kt
+++ app/src/main/java/com/example/returnpals/mainMenu/MainMenu.kt
@@ -27,6 +27,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -132,8 +134,13 @@ fun DrawerItem(title: String, isSelected: Boolean, onClick: () -> Unit) {
     ) {
         Text(
             text = title,
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                // fontFamily = customFont
+            ),
             color = if (isSelected) selectedBlue else Color.Black,
             modifier = Modifier.padding(2.dp)
+
         )
 
     }
