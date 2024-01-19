812d3bdf1d0464d0f3233761fc51ca7eda0ef07b
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-01-18 23:19:19 -0800

    Add empty test class

---
+++ app/src/androidTest/kotlin/com/example/returnpals/composetools/IconManagerTest.kt
@@ -0,0 +1,84 @@
+package com.example.returnpals.composetools
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class IconManagerTest {
+
+    @Test
+    fun getAmazonIcon() {
+    }
+
+    @Test
+    fun getBarcodeIcon() {
+    }
+
+    @Test
+    fun getBestchoiceIcon() {
+    }
+
+    @Test
+    fun getBoxIcon() {
+    }
+
+    @Test
+    fun getBoxholdIcon() {
+    }
+
+    @Test
+    fun getBoxliftIcon() {
+    }
+
+    @Test
+    fun getCalendarIcon() {
+    }
+
+    @Test
+    fun getCheckmarkIcon() {
+    }
+
+    @Test
+    fun getComputerIcon() {
+    }
+
+    @Test
+    fun getDoorstepIcon() {
+    }
+
+    @Test
+    fun getFileIcon() {
+    }
+
+    @Test
+    fun getHandoffIcon() {
+    }
+
+    @Test
+    fun getLabelIcon() {
+    }
+
+    @Test
+    fun getPinIcon() {
+    }
+
+    @Test
+    fun getReturnPalNameIcon() {
+    }
+
+    @Test
+    fun getReturnPalIcon() {
+    }
+
+    @Test
+    fun getStopwatchIcon() {
+    }
+
+    @Test
+    fun getTruckIcon() {
+    }
+
+    @Test
+    fun getVanIcon() {
+    }
+}
