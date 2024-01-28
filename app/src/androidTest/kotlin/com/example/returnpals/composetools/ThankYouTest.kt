888c911b25b84f99a0efa0457079a06e915436d1
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-01-28 15:02:47 -0800

    add empty rule setContent

--- app/src/androidTest/kotlin/com/example/returnpals/composetools/ThankYouTest.kt
+++ app/src/androidTest/kotlin/com/example/returnpals/composetools/ThankYouTest.kt
@@ -1,37 +1,68 @@
 package com.example.returnpals.composetools
 
+import androidx.compose.ui.test.junit4.createComposeRule
 import org.junit.Assert.*
+import org.junit.Rule
 
 import org.junit.Test
 
 class ThankYouTest {
+    @get:Rule
+    val rule = createComposeRule()
 
     @Test
     fun progressBarChecks() {
+        rule.setContent {
+
+        }
     }
     @Test
     fun truckIcon() {
+        rule.setContent {
+
+        }
     }
     @Test
     fun thankYouText() {
+        rule.setContent {
+
+        }
     }
     @Test
     fun thankYouTextColors() {
+        rule.setContent {
+
+        }
     }
     @Test
     fun confirmText() {
+        rule.setContent {
+
+        }
     }
     @Test
     fun confirmTextColors() {
+        rule.setContent {
+
+        }
     }
     @Test
     fun emailText() {
+        rule.setContent {
+
+        }
     }
     @Test
     fun emailTextColors() {
+        rule.setContent {
+
+        }
     }
     @Test
     fun dashBoardButton() {
+        rule.setContent {
+
+        }
     }
 
 }
