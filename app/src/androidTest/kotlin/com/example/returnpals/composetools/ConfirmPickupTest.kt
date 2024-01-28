e47a72488c09d7c9c32f74d015acb944ff480fa1
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-01-28 15:48:34 -0800

    add set content rule

--- app/src/androidTest/kotlin/com/example/returnpals/composetools/ConfirmPickupTest.kt
+++ app/src/androidTest/kotlin/com/example/returnpals/composetools/ConfirmPickupTest.kt
@@ -13,46 +13,87 @@ class ConfirmPickupTest {
 
     @Test
     fun progressBarChecks() {
-        rule.setContent {  }
+        rule.setContent {
+           ConfirmPickup().drawConfirmPickup(nextButton = {}, backButton = {}) {}
+        }
     }
     @Test
     fun confimYourPickupText() {
+        rule.setContent {
+            ConfirmPickup().drawConfirmPickup(nextButton = {}, backButton = {}) {}
+        }
     }
 
     @Test
     fun orderSummeryText() {
+        rule.setContent {
+            ConfirmPickup().drawConfirmPickup(nextButton = {}, backButton = {}) {}
+        }
     }
     @Test
     fun calendarDates() {
+        rule.setContent {
+            ConfirmPickup().drawConfirmPickup(nextButton = {}, backButton = {}) {}
+        }
     }
     @Test
     fun pickupTypeChecks() {
+        rule.setContent {
+            ConfirmPickup().drawConfirmPickup(nextButton = {}, backButton = {}) {}
+        }
     }
     @Test
     fun addresschecks() {
+        rule.setContent {
+            ConfirmPickup().drawConfirmPickup(nextButton = {}, backButton = {}) {}
+        }
     }
     @Test
     fun numberOfLabelsDigital() {
+        rule.setContent {
+            ConfirmPickup().drawConfirmPickup(nextButton = {}, backButton = {}) {}
+        }
     }
     @Test
     fun numberOfLabelsPhysical() {
+        rule.setContent {
+            ConfirmPickup().drawConfirmPickup(nextButton = {}, backButton = {}) {}
+        }
     }
     @Test
     fun numberOfLabelsIcons() {
+        rule.setContent {
+            ConfirmPickup().drawConfirmPickup(nextButton = {}, backButton = {}) {}
+        }
     }
     @Test
     fun cardCheck() {
+        rule.setContent {
+            ConfirmPickup().drawConfirmPickup(nextButton = {}, backButton = {}) {}
+        }
     }
     @Test
     fun priceChecks() {
+        rule.setContent {
+            ConfirmPickup().drawConfirmPickup(nextButton = {}, backButton = {}) {}
+        }
     }
     @Test
     fun nextButton() {
+        rule.setContent {
+            ConfirmPickup().drawConfirmPickup(nextButton = {}, backButton = {}) {}
+        }
     }
     @Test
     fun backButton() {
+        rule.setContent {
+            ConfirmPickup().drawConfirmPickup(nextButton = {}, backButton = {}) {}
+        }
     }
     @Test
     fun promoChecks() {
+        rule.setContent {
+            ConfirmPickup().drawConfirmPickup(nextButton = {}, backButton = {}) {}
+        }
     }
 }
