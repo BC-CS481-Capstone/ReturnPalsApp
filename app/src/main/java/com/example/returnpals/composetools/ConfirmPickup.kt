6f8d003e3a7e6b6a74c0e1335f650b25d3059df9
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-01-28 00:56:39 -0800

    Create empty drawConfirmPickup function

--- app/src/main/java/com/example/returnpals/composetools/ConfirmPickup.kt
+++ app/src/main/java/com/example/returnpals/composetools/ConfirmPickup.kt
@@ -1,4 +1,27 @@
 package com.example.returnpals.composetools
 
+import android.location.Address
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import java.util.Date
+import java.util.Locale
+
 class ConfirmPickup {
+
+    @Composable
+    fun drawConfirmPickup(numberOfDigitial: Int = 0,
+                          numberOfPhysical: Int = 0,
+                          visaLastFour: Int = 5555,
+                          pickUpDate: Date = Date(),
+                          pickUpAddress: Address = Address(Locale.CANADA),
+                          priceArray: Array<Int> = arrayOf(0,0,0),
+                          nextButton:() -> Unit,
+                          backButton: () -> Unit,
+                          promoButton: () -> Unit)
+    {
+
+    }
+
 }
