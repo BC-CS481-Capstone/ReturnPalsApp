86a8b715afde07525859a03c833c835a4cc65fa8
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-01-28 09:09:21 -0800

    add font scaling

--- app/src/main/java/com/example/returnpals/composetools/ConfirmPickup.kt
+++ app/src/main/java/com/example/returnpals/composetools/ConfirmPickup.kt
@@ -1,16 +1,28 @@
 package com.example.returnpals.composetools
 
 import android.location.Address
+import android.os.Build
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.security.AllPermission
+import java.time.DayOfWeek
 import java.util.Calendar
 import java.util.Date
 import java.util.Locale
@@ -30,14 +42,25 @@ class ConfirmPickup {
                           promoButton: () -> Unit)
     {
         // Set up as a row with progress on top
-        Column(Modifier.background(getBackGroundColor()), verticalArrangement = Arrangement.SpaceBetween, horizontalAlignment = Alignment.CenterHorizontally ) {
+        Column(
+            Modifier
+                .background(getBackGroundColor())
+                .fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween, horizontalAlignment = Alignment.CenterHorizontally ) {
             ProgressBar(modifier = Modifier, step = 4)
-            Text("Confirm Your Pickup",Modifier)
-            Column(Modifier.background(Color.White,shape = RoundedCornerShape(10) )) {
-                val day = pickUpDate.get(Calendar.DAY_OF_WEEK)
+            Text("Confirm Your Pickup",Modifier .fillMaxWidth(),color = Color.Black, textAlign = TextAlign.Center, fontWeight = FontWeight.Bold,fontSize = 38.sp,maxLines = 1)
+            Column(Modifier.background(Color.White,shape = RoundedCornerShape(10) ),verticalArrangement = Arrangement.SpaceAround, horizontalAlignment = Alignment.CenterHorizontally ) {
+                Text("Order Summary\n" +
+                        "_________________",color = getBlueIconColor(),fontSize = 34.sp, maxLines = 2)
+
+                val day = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    DayOfWeek.of(pickUpDate.get(Calendar.DAY_OF_WEEK))
+                } else {
+                    TODO("VERSION.SDK_INT < O")
+                }
                 val month = pickUpDate.get(Calendar.MONTH)
                 val date = pickUpDate.get(Calendar.DATE)
-                Text("pickUpDate")
+
+                Text(day.toString()+month.toString()+date.toString(),Modifier)
                 Text(typeOfPickup)
                 if (pickUpAddress.getAddressLine(0) != null ) {
                     Text(text = pickUpAddress.getAddressLine(0))
@@ -49,15 +72,23 @@ class ConfirmPickup {
                     Text(text = pickUpAddress.getAddressLine(2))
                 }
                 Text("Packages")
-                Text("Package with digital label")
-                Text("Package with physical label")
-                Text("Visa ending")
+                Row(horizontalArrangement =  Arrangement.Start, verticalAlignment = Alignment.CenterVertically) {
+                    IconManager().getBoxIcon(modifier = Modifier.height(22.dp))
+                    Text(numberOfDigitial.toString()+" Package with digital label",fontSize = 18.sp,maxLines = 1)
+                }
+                Row(horizontalArrangement =  Arrangement.Start, verticalAlignment = Alignment.CenterVertically) {
+                    IconManager().getBoxIcon(modifier = Modifier.height(22.dp))
+                    Text(numberOfPhysical.toString()+" Package with physical label",fontSize = 18.sp,maxLines = 1)
+                }
+
                 if (priceArray[0]!=0) {
+                    Text("Visa ending")
                     Text("One-Time Return")
                     Text("Tax")
                     Text("Total")
+                    Text("Promo and Apply")
                 }
-                Text("Promo and Apply")
+
             }
             Row(Modifier) {
                 ButtonManager.BackButton(onClick = backButton)
