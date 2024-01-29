35a1ce37cc73ed6494e7cd3e84fd0215b9ccce32
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-01-28 18:33:30 -0800

    add code commits

--- app/src/main/java/com/example/returnpals/composetools/ThankYou.kt
+++ app/src/main/java/com/example/returnpals/composetools/ThankYou.kt
@@ -14,13 +14,23 @@ import androidx.compose.ui.unit.dp
 class ThankYou {
 
     @Composable
-    fun drawThankYouUI(userName:String = "Guest",confirmNumber:String = "#R957394",email:String = "johndoe2394@gmail.com",dashBoardButton: () -> Unit) {
+    fun drawThankYouUI(userName:String = "Guest",
+                       confirmNumber:String = "#R957394",
+                       email:String = "johndoe2394@gmail.com",
+                       dashBoardButton: () -> Unit)
+    {
+        //Column with progress at top
         Column(Modifier.fillMaxSize(),verticalArrangement = Arrangement.SpaceBetween, horizontalAlignment = Alignment.CenterHorizontally) {
             ProgressBar(step = 5)
+            //Truck icon
             IconManager().getTruckIcon(modifier = Modifier.height(36.dp))
+            //Thank you text
             Text("Thank you for scheduling a return with us!")
+            //Confirm order number
             Text(userName+", your pickup order "+confirmNumber+" is confirmed.")
+            //Confirm email
             Text("A confirmation email will be sent to "+email)
+            //Navigation button out
             ButtonManager.NextButton(onClick =dashBoardButton, text = "Return to Dashboard")
         }
     }
