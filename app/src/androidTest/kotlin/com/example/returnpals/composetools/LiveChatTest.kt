de6924390089df868800f49cf67791e0ed5ccbc1
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-01-29 00:08:40 -0800

    Add empty test class

---
+++ app/src/androidTest/kotlin/com/example/returnpals/composetools/LiveChatTest.kt
@@ -0,0 +1,20 @@
+package com.example.returnpals.composetools
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class LiveChatTest {
+
+    @Test
+    fun drawChatFrame() {
+    }
+
+    @Test
+    fun drawChatScroll() {
+    }
+
+    @Test
+    fun drawChatInput() {
+    }
+}
