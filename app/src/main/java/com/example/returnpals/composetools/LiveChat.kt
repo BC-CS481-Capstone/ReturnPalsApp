058036b5000e430d722ef73e0c047279e955a954
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-01-28 20:07:27 -0800

    create empty LiveChat class

---
+++ app/src/main/java/com/example/returnpals/composetools/LiveChat.kt
@@ -0,0 +1,13 @@
+package com.example.returnpals.composetools
+
+import androidx.compose.runtime.Composable
+
+class LiveChat {
+    @Composable
+    fun drawChatFrame() {
+
+    }
+    fun drawChatScroll() {
+
+    }
+}
