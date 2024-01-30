f49f7aec415aaa0172e0a056e0ed8e19e0f25af7
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-01-29 19:38:26 -0800

    empty test functions

--- app/src/androidTest/kotlin/com/example/returnpals/composetools/LiveChatTest.kt
+++ app/src/androidTest/kotlin/com/example/returnpals/composetools/LiveChatTest.kt
@@ -1,20 +1,43 @@
 package com.example.returnpals.composetools
 
+import androidx.compose.ui.test.junit4.createComposeRule
 import org.junit.Assert.*
+import org.junit.Rule
 
 import org.junit.Test
 
 class LiveChatTest {
+    @get:Rule
+    val rule = createComposeRule()
 
     @Test
-    fun drawChatFrame() {
+    fun sendMessage() {
+        rule.setContent{
+            //LiveChat().drawChatFrame(send = { /*TODO*/ }, message = , messages = )
+        }
     }
 
     @Test
-    fun drawChatScroll() {
+    fun readMessages() {
+        rule.setContent {}
     }
 
     @Test
-    fun drawChatInput() {
+    fun sendButton() {
+        rule.setContent {}
     }
+
+    @Test
+    fun textField() {
+        rule.setContent {}
+    }
+    @Test
+    fun iconChecks() {
+        rule.setContent {}
+    }
+    @Test
+    fun iconChecks2() {
+        rule.setContent {}
+    }
+
 }
