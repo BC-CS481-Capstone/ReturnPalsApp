b4daa81326321e7bde8220f2d675ebc01d0773cf
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-02-02 02:14:28 -0800

    add annotatedString fun

--- app/src/main/java/com/example/returnpals/composetools/ThankYou.kt
+++ app/src/main/java/com/example/returnpals/composetools/ThankYou.kt
@@ -8,6 +8,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import com.example.returnpals.composetools.ButtonManager
 import androidx.compose.ui.unit.dp
 
@@ -32,6 +34,46 @@ class ThankYou {
             Text("A confirmation email will be sent to "+email)
             //Navigation button out
             ButtonManager.NextButton(onClick =dashBoardButton, text = "Return to Dashboard")
+        }
+    }
+
+
+    fun thankText() {
+        //Builds annotated string for /BLUE/ thank you /NORMAL/ for scheduling a return with us
+        buildAnnotatedString {
+            pushStyle(style = SpanStyle(color = getBlueIconColor())) //push style onto stack
+            append("Thank you")
+            pop() //pop style off stack
+            append("for scheduling a return with us!")
+        }
+    }
+
+
+    fun confirmNumberText(name:String,
+                          confirm:String,
+                          )
+    {
+        //Builds annotated string for /NORMAL/ %NAME%, your pickup order /BLUE/ %CONFIRM#% /NORMAL/ is confirmed.
+        buildAnnotatedString {
+            append(name)
+            append("your pickup order")
+            pushStyle(style= SpanStyle(color= getBlueIconColor()))  // Push style onto stack
+            append(confirm)
+            pop() //pop style off stack
+            append("is confirmed.")
+        }
+    }
+
+
+    fun emailText(email:String)
+    {
+        //Builds annotated string for /NORMAL/ A confirmation email will be sent to: /BLUE/ %EMAIL%
+        buildAnnotatedString {
+            append("A confirmation email will be sent to:")
+            pushStyle(style = SpanStyle(color = getBlueIconColor())) //push style onto stack
+            append(email)
+            pop() //pop style off stack
+
         }
     }
 }
