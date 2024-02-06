9a0575c8770759bad339ca0deea15f8ea726dbc9
andrew-pogue <andrewthomaspogue@gmail.com>
2024-02-06 14:12:17 -0800

    rewrote ScheduleReturn into composable

--- app/src/main/java/com/example/returnpals/ScheduleReturn.kt
+++ app/src/main/java/com/example/returnpals/ScheduleReturn.kt
@@ -1,8 +1,18 @@
 package com.example.returnpals
 
 import android.location.Address
-import android.os.Build
-import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.navigation.NavController
+import com.example.returnpals.composetools.ConfirmPickup
+import com.example.returnpals.composetools.PackagesUI
+import com.example.returnpals.composetools.PickupDateUI
+import com.example.returnpals.composetools.PickupMethodUI
+import com.example.returnpals.composetools.PricingUI
+import com.example.returnpals.composetools.ThankYou
+import com.example.returnpals.composetools.dashboard.SelectAddressContent
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -15,69 +25,127 @@ import java.time.LocalDate
  * https://kotlinlang.org/docs/extensions.html#extension-functions
  */
 
-/**
- * This object is extended with ui functions defined in `com.example.returnpals.composetools`.
- * It's used as a namespace, grouping together features related to the "Schedule a Return" action.
- */
-object ScheduleReturn {
+/////////////////////////////////////////////////////////////////////////////
+// PUBLIC API
+////////////////////
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    class ViewModel(init: PickupInfo) : androidx.lifecycle.ViewModel() {
+object ScheduleReturn {}
 
-        private val idManager: IdManager = IdManager()
-        private val _pickup: MutableStateFlow<PickupInfo>
-        val pickup: StateFlow<PickupInfo>
+@Composable
+fun ScheduleReturn(
+    navController: NavController,
+    pickupInfo: PickupInfo,
+) {
 
-        init {
-            _pickup = MutableStateFlow(init)
-            pickup = _pickup.asStateFlow()
-            // _pickup.value and pickup.value should NOT be null after init
+    val viewModel = remember { ScheduleReturnViewModel(pickupInfo) }
+    val step = remember { mutableIntStateOf(1) }
+    val state = viewModel.pickup.collectAsState();
+
+    val confirm = ConfirmPickup()
+    val thanks = ThankYou()
+
+    when (step.intValue) {
+        1 -> ScheduleReturn.PickupDateUI(
+                navController = navController,
+                date = state.value.date,
+//                onClickNext = { step.intValue++ },
+//                onClickBack = { step.intValue-- },
+            )
+        2 -> SelectAddressContent(
+                navController = navController,
+            )
+        3 -> ScheduleReturn.PickupMethodUI(
+                navController = navController,
+                method = state.value.method,
+            )
+        4 -> ScheduleReturn.PickupMethodUI(
+                navController = navController,
+                method = state.value.method,
+            )
+        5 -> ScheduleReturn.PricingUI(
+                navController = navController,
+                plan = state.value.pricing,
+            )
+        6 -> ScheduleReturn.PackagesUI(
+                navController = navController,
+                packages = state.value.packages.values.toList(),
+            )
+        7 -> confirm.drawConfirmPickup(
+                typeOfPickup = state.value.method.toString(),
+//            TODO: change type of pickupDate parameter to LocalDate
+//               OR change type of PackageInfo.date to Calendar
+//            TODO: show pricing only if BRONZE plan is selected
+                pickUpAddress = state.value.address!!,
+                nextButton = { step.intValue++ },
+                backButton = { step.intValue-- },
+                promoButton = {}
+            )
+        8 -> thanks.drawThankYouUI(
+                dashBoardButton = { /* TODO: navigate to dashboard */ },
+            )
+    }
+
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// PRIVATE API
+////////////////////
+// other files need not be aware of or concerned with everything below here
+
+private class ScheduleReturnViewModel(init: PickupInfo) : androidx.lifecycle.ViewModel() {
+
+    private val idManager: IdManager = IdManager()
+    private val _pickup: MutableStateFlow<PickupInfo>
+    val pickup: StateFlow<PickupInfo>
+
+    init {
+        _pickup = MutableStateFlow(init)
+        pickup = _pickup.asStateFlow()
+        // _pickup.value and pickup.value should NOT be null after init
+    }
+
+    fun onChangeDate(value: LocalDate) {
+        _pickup.update {
+            it.date = value
+            it
         }
+    }
 
-        fun onChangeDate(value: LocalDate) {
-            _pickup.update {
-                it.date = value
-                it
-            }
+    fun onChangeAddress(value: Address) {
+        _pickup.update {
+            it.address = value
+            it
         }
+    }
 
-        fun onChangeAddress(value: Address) {
-            _pickup.update {
-                it.address = value
-                it
-            }
+    fun onChangePickupMethod(value: PickupMethod) {
+        _pickup.update {
+            it.method = value
+            it
         }
+    }
 
-        fun onChangePickupMethod(value: PickupMethod) {
-            _pickup.update {
-                it.method = value
-                it
-            }
+    fun onAddLabel(value: PackageInfo) {
+        _pickup.update {
+            val label = value.copy(id=idManager.allot())
+            it.packages[label.id] = label
+            it
         }
+    }
 
-        fun onAddLabel(value: PackageInfo) {
-            _pickup.update {
-                val label = value.copy(id=idManager.allot())
-                it.packages[label.id] = label
-                it
-            }
+    fun onRemoveLabel(id: Long) {
+        _pickup.update {
+            it.packages.remove(id)
+            idManager.free(id)
+            it
         }
+    }
 
-        fun onRemoveLabel(id: Long) {
-            _pickup.update {
-                it.packages.remove(id)
-                idManager.free(id)
-                it
-            }
+    fun onChangePricingPlan(value: PricingPlan) {
+        _pickup.update {
+            it.pricing = value
+            it
         }
-
-        fun onChangePricingPlan(value: PricingPlan) {
-            _pickup.update {
-                it.pricing = value
-                it
-            }
-        }
-
     }
 
 }
