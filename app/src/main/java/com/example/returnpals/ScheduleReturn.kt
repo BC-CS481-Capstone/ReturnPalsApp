8e651c1599a9251ba570a65ad067ddf2ee34c6e7
andrew-pogue <andrewthomaspogue@gmail.com>
2024-02-09 20:37:06 -0800

    rework ScheduleReturn

--- app/src/main/java/com/example/returnpals/ScheduleReturn.kt
+++ app/src/main/java/com/example/returnpals/ScheduleReturn.kt
@@ -1,22 +1,27 @@
 package com.example.returnpals
 
 import android.location.Address
+import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.returnpals.composetools.PickupMethodUI
 import com.example.returnpals.composetools.ConfirmPickup
 import com.example.returnpals.composetools.PackagesUI
 import com.example.returnpals.composetools.PickupDateUI
 import com.example.returnpals.composetools.PricingUI
 import com.example.returnpals.composetools.ThankYou
-import com.example.returnpals.composetools.dashboard.SelectAddressContent
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import com.example.returnpals.composetools.goto
+import com.example.returnpals.mainMenu.MenuRoutes
 import java.time.LocalDate
 
 /**
@@ -29,67 +34,89 @@ import java.time.LocalDate
 // PUBLIC API
 ////////////////////
 
-object ScheduleReturn {}
+enum class ScheduleReturnStep {
+    SelectDate,
+    SelectAddress,
+    SelectMethod,
+    SelectPricing,
+    AddLabels,
+    Confirmation,
+    Thanks;
+
+    operator fun inc(): ScheduleReturnStep {
+//        if (this == Thanks) throw overflow error
+        return ScheduleReturnStep.values()[this.ordinal+1]
+    }
+
+    operator fun dec(): ScheduleReturnStep {
+//        if (this == Thanks) throw overflow error
+        return ScheduleReturnStep.values()[this.ordinal-1]
+    }
+}
+
+@Composable
+fun DemoScheduleReturn() {
+    val controller = rememberNavController()
+    ScheduleReturn(
+        navController = controller,
+    )
+}
 
 @Composable
 fun ScheduleReturn(
     navController: NavController,
-    pickupInfo: PickupInfo,
 ) {
+    val step = remember { mutableStateOf(ScheduleReturnStep.values()[0]) }
+    val pickupVM = PickupViewModel(PickupInfo())
 
-    val viewModel = remember { ScheduleReturnViewModel(pickupInfo) }
-    val step = remember { mutableIntStateOf(1) }
-    val state = viewModel.pickup.collectAsState();
+    ScheduleReturnContent(step.value, pickupVM,
+        onClickNext = { step.value++; Log.println(Log.INFO, "onClickNext", step.value.toString()) },
+        onClickBack = { step.value--; Log.println(Log.INFO, "onClickBack", step.value.toString()) },
+        onSubmit = { goto(navController, MenuRoutes.HomeDash) }
+    )
 
-    val confirm = ConfirmPickup()
-    val thanks = ThankYou()
+}
 
-    val minDate = LocalDate.now().minusDays(1)
-    val maxDate = LocalDate.now().plusMonths(2)
+open class PickupViewModel(
+    pickup: PickupInfo,
+) : ViewModel() {
 
-    when (step.intValue) {
-        1 -> PickupDateUI(
-                date = state.value.date,
-                onChangeDate = { state.value.date = it },
-                onClickNext = { step.intValue++ },
-                onClickBack = { step.intValue-- },
-                isValidDate = { state.value.date > minDate && state.value.date < maxDate }
-            )
-        2 -> SelectAddressContent(
-                navController = navController,
-            )
-        3 -> PickupMethodUI(
-                method = state.value.method,
-                onClickNext = {},
-                onClickBack = {},
-                onChangeMethod = {},
-            )
-        4 -> PricingUI(
-                onClickNext = {},
-                onClickBack = {},
-                onChangePlan = {},
-                plan = state.value.pricing,
-            )
-        5 -> PackagesUI(
-                packages = state.value.packages.values.toList(),
-                onAddLabel = {},
-                onRemoveLabel = {},
-                onClickNext = {},
-                onClickBack = {},
-            )
-        6 -> confirm.drawConfirmPickup(
-                typeOfPickup = state.value.method.toString(),
-//            TODO: change type of pickupDate parameter to LocalDate
-//               OR change type of PackageInfo.date to Calendar
-//            TODO: show pricing only if BRONZE plan is selected
-                pickUpAddress = state.value.address!!,
-                nextButton = { step.intValue++ },
-                backButton = { step.intValue-- },
-                promoButton = {}
-            )
-        7 -> thanks.drawThankYouUI(
-                dashBoardButton = { /* TODO: navigate to dashboard */ },
-            )
+    private val _idManager: IdManager = IdManager()
+    private val _data: MutableLiveData<PickupInfo> = MutableLiveData(pickup)
+    val data: LiveData<PickupInfo> = _data
+    private val _minDate: LocalDate = LocalDate.now().minusDays(1)
+    private val _maxDate: LocalDate = LocalDate.now().plusMonths(2)
+
+    // not sure why _data.value is nullable...
+
+    fun isValidDate(value: LocalDate): Boolean {
+        return value > _minDate && value < _maxDate
+    }
+
+    fun onChangeDate(value: LocalDate) {
+        if (isValidDate(value)) _data.value!!.date = value
+    }
+
+    fun onChangeAddress(value: Address) {
+        _data.value!!.address = value
+    }
+
+    fun onChangeMethod(value: PickupMethod) {
+        _data.value!!.method = value
+    }
+
+    fun onAddLabel(value: PackageInfo) {
+        val id = _idManager.allot()
+        _data.value!!.packages[id] = value.copy(id=id)
+    }
+
+    fun onRemoveLabel(id: Long) {
+        _data.value!!.packages.remove(id)
+        _idManager.free(id)
+    }
+
+    fun onChangePlan(value: PricingPlan) {
+        _data.value!!.pricing = value
     }
 
 }
@@ -97,62 +124,71 @@ fun ScheduleReturn(
 /////////////////////////////////////////////////////////////////////////////
 // PRIVATE API
 ////////////////////
-// other files need not be aware of or concerned with everything below here
 
-private class ScheduleReturnViewModel(init: PickupInfo) : androidx.lifecycle.ViewModel() {
+@Preview
+@Composable
+private fun ScheduleReturnPreview() {
+    ScheduleReturnContent(
+        ScheduleReturnStep.AddLabels,
+        PickupViewModel(PickupInfo()),
+        onClickNext = {},
+        onClickBack = {},
+        onSubmit = {}
+    )
+}
 
-    private val idManager: IdManager = IdManager()
-    private val _pickup: MutableStateFlow<PickupInfo>
-    val pickup: StateFlow<PickupInfo>
+@Composable
+private fun ScheduleReturnContent(
+    step: ScheduleReturnStep,
+    pickupVM: PickupViewModel,
+    onClickNext: () -> Unit,
+    onClickBack: () -> Unit,
+    onSubmit: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val pickup : PickupInfo by pickupVM.data.observeAsState(PickupInfo())
+    val confirm = ConfirmPickup()
 
-    init {
-        _pickup = MutableStateFlow(init)
-        pickup = _pickup.asStateFlow()
-        // _pickup.value and pickup.value should NOT be null after init
-    }
-
-    fun onChangeDate(value: LocalDate) {
-        _pickup.update {
-            it.date = value
-            it
-        }
-    }
-
-    fun onChangeAddress(value: Address) {
-        _pickup.update {
-            it.address = value
-            it
-        }
-    }
-
-    fun onChangePickupMethod(value: PickupMethod) {
-        _pickup.update {
-            it.method = value
-            it
-        }
-    }
-
-    fun onAddLabel(value: PackageInfo) {
-        _pickup.update {
-            val label = value.copy(id=idManager.allot())
-            it.packages[label.id] = label
-            it
-        }
-    }
-
-    fun onRemoveLabel(id: Long) {
-        _pickup.update {
-            it.packages.remove(id)
-            idManager.free(id)
-            it
-        }
-    }
-
-    fun onChangePricingPlan(value: PricingPlan) {
-        _pickup.update {
-            it.pricing = value
-            it
-        }
+    when (step) {
+        ScheduleReturnStep.SelectDate -> PickupDateUI(
+            date = pickup.date,
+            onChangeDate = pickupVM::onChangeDate,
+            isValidDate = pickupVM::isValidDate,
+            onClickNext = { onClickNext(); onClickNext() },
+            onClickBack = onClickBack,
+            modifier = modifier)
+        ScheduleReturnStep.SelectAddress -> onClickNext()
+//            SelectAddressContent(navController = navController)
+        ScheduleReturnStep.SelectMethod -> PickupMethodUI(
+            method = pickup.method,
+            onChangeMethod = pickupVM::onChangeMethod,
+            onClickNext = onClickNext,
+            onClickBack = { onClickBack(); onClickBack() },
+            modifier = modifier)
+        ScheduleReturnStep.SelectPricing -> PricingUI(
+            plan = pickup.pricing,
+            onChangePlan = pickupVM::onChangePlan,
+            onClickNext = onClickNext,
+            onClickBack = onClickBack,
+            modifier = modifier)
+        ScheduleReturnStep.AddLabels -> PackagesUI(
+            packages = pickup.packages.values.toList(),
+            onAddLabel = pickupVM::onAddLabel,
+            onRemoveLabel = pickupVM::onRemoveLabel,
+            onClickNext = onClickNext,
+            onClickBack = onClickBack)
+        ScheduleReturnStep.Confirmation -> confirm.drawConfirmPickup(
+            typeOfPickup = pickup.method.toString(),
+//            TODO: change type of pickupDate parameter to LocalDate
+//               OR change type of PackageInfo.date to Calendar
+//            TODO: show pricing only if BRONZE plan is selected
+            pickUpAddress = pickup.address!!,
+            nextButton = onClickNext,
+            backButton = onClickBack,
+            promoButton = {})
+        ScheduleReturnStep.Thanks -> ThankYou().drawThankYouUI(
+            dashBoardButton = onSubmit
+        )
     }
 
 }
