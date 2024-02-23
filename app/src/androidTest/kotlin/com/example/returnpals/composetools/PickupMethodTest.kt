70b8ebac64e8b1bd276b066a93a6b374083e1cf7
andrew-pogue <andrewthomaspogue@gmail.com>
2024-02-23 00:55:27 -0800

    refactored pickup method test

--- app/src/androidTest/kotlin/com/example/returnpals/composetools/PickupMethodTest.kt
+++ app/src/androidTest/kotlin/com/example/returnpals/composetools/PickupMethodTest.kt
@@ -9,67 +9,107 @@ import com.example.returnpals.PickupMethod
 import org.junit.Rule
 import org.junit.Test
 
+/**
+ * Tests the [PickupMethodUI] composable.
+ */
 class PickupMethodTest {
 
     @get:Rule
     val rule = createComposeRule()
 
     @Test
-    fun testPickupMethodUI() {
+    fun testNextButton() {
         val method = mutableStateOf<PickupMethod?>(null)
         var isClicked: Boolean
 
         rule.setContent {
             PickupMethodUI(
                 method = method.value,
-                onChangeMethod = { method.value = it },
-                onClickBack = { isClicked = true },
+                onChangeMethod = { },
+                onClickBack = { },
                 onClickNext = { isClicked = true },
             )
         }
 
-        // Test next and back buttons:
-
         val next = rule.onNodeWithText("Next")
-        val back = rule.onNodeWithText("Back")
-
-        next.assertExists("Next button does not exist")
-        back.assertExists("Back button does not exist")
-
+        next.assertExists("Next button does not exist.")
         next.assertIsDisplayed()
-        back.assertIsDisplayed()
-
-        isClicked = false
-        next.performClick()
-        assert(!isClicked) { "Next button not disabled when no pickup method is selected." }
 
         method.value = PickupMethod.DOORSTEP
         isClicked = false
         next.performClick()
-        assert(isClicked) { "Next button not enabled when pickup method is selected." }
+        assert(isClicked) { "Next button on-click event does not work." }
+
+        method.value = null
+        isClicked = false
+        next.performClick()
+        assert(!isClicked) { "Next button enabled when no valid option is selected." }
+    }
+
+    @Test
+    fun testBackButton() {
+        var isClicked: Boolean
+
+        rule.setContent {
+            PickupMethodUI(
+                method = null,
+                onChangeMethod = { },
+                onClickBack = { isClicked = true },
+                onClickNext = { },
+            )
+        }
+
+        val back = rule.onNodeWithText("Back")
+        back.assertExists("Back button does not exist.")
+        back.assertIsDisplayed()
 
         isClicked = false
         back.performClick()
-        assert(isClicked) { "Back button does not work." }
+        assert(isClicked) { "Back button on-click event does not work." }
+    }
 
-        // Test pickup method options:
+    @Test
+    fun testDoorstepOption() {
+        val method = mutableStateOf<PickupMethod?>(null)
+
+        rule.setContent {
+            PickupMethodUI(
+                method = method.value,
+                onChangeMethod = { method.value = it },
+                onClickBack = { },
+                onClickNext = { },
+            )
+        }
 
         val doorstep = rule.onNodeWithText("Leave on Doorstep")
-        val handoff = rule.onNodeWithText("Direct Handoff")
-
-        doorstep.assertExists("Doorstep option does not exist.")
-        handoff.assertExists("Handoff option does not exist.")
-
+        doorstep.assertExists("\"Leave on Doorstep\" option does not exist.")
         doorstep.assertIsDisplayed()
-        handoff.assertIsDisplayed()
 
         method.value = null
         doorstep.performClick()
-        assert(method.value == PickupMethod.DOORSTEP) { "Doorstep option's onclick does not work." }
+        assert(method.value == PickupMethod.DOORSTEP) { "\"Leave on Doorstep\" option on-click event does not work." }
+    }
+
+    @Test
+    fun testHandoffOption() {
+        val method = mutableStateOf<PickupMethod?>(null)
+
+        rule.setContent {
+            PickupMethodUI(
+                method = method.value,
+                onChangeMethod = { method.value = it },
+                onClickBack = { },
+                onClickNext = { },
+            )
+        }
+
+        val handoff = rule.onNodeWithText("Direct Handoff")
+        handoff.assertExists("\"Direct Handoff\" option does not exist.")
+        handoff.assertIsDisplayed()
 
         method.value = null
         handoff.performClick()
-        assert(method.value == PickupMethod.HANDOFF) { "Doorstep option's onclick does not work." }
+        assert(method.value == PickupMethod.HANDOFF) { "\"Direct Handoff\" option on-click event does not work." }
     }
 
 }
