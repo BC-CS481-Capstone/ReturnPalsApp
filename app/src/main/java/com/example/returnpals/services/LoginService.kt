ecd44255ef5d0a2e700086ce174bf45224924d88
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-02-24 01:07:30 -0800

    add amplify api

--- app/src/main/java/com/example/returnpals/services/LoginService.kt
+++ app/src/main/java/com/example/returnpals/services/LoginService.kt
@@ -1,21 +1,57 @@
 package com.example.returnpals.services
 
-class LoginService {
-    fun encryptPass(pass:String):String {
-        //Encrypts user passwords
-        return pass
+import android.util.Log
+import com.amplifyframework.auth.AuthException
+import com.amplifyframework.auth.AuthUserAttributeKey
+import com.amplifyframework.auth.options.AuthSignUpOptions
+import com.amplifyframework.auth.result.AuthSignInResult
+import com.amplifyframework.auth.result.AuthSignOutResult
+import com.amplifyframework.core.Amplify
+
+//** @LoginService.kt
+// This file create the functions to signUp, login, and logout of the cognito.
+// */
+
+    fun singUp(userName:String,passWord:String,email:String) {
+        //Allows users to signup with cognito
+        Amplify.Auth.signUp(
+            userName,
+            passWord,
+            AuthSignUpOptions.builder().userAttribute(AuthUserAttributeKey.email(), email)
+                .build(),
+            { result -> Log.i("Amplify Auth", "Result: " + result.toString()) }
+        ) { error -> Log.e("Amplify Auth", "Sign up failed", error) }
     }
 
-    fun isEmail(email:String):Boolean {
-        //Boolean test for valid email format  example@server.com
-        return false
+
+    fun signOut(email:String) {
+        //Allows users to signout of cognito
+        Amplify.Auth.signOut { onComplete: AuthSignOutResult? ->
+            Log.i(
+                "Amplify Auth",
+                "Signed out successfully completed"
+            )
+        }
     }
 
-    fun login(user:String,pass:String):Boolean{
-        //Checks user's password and email to the server and verifies if logged in.
-        return false
+    fun logIn(user:String,pass:String){
+        // Allows users to signIn with cognito
+        Amplify.Auth.signIn(
+            user,
+            pass,
+            { result: AuthSignInResult ->
+                Log.i(
+                    "Amplify Auth",
+                    if (result.isSignedIn) "Sign in succeeded" else "Sign in not complete"
+                )
+            }
+        ) { error: AuthException ->
+            Log.e(
+                "Amplify Auth",
+                error.toString()
+            )
+        }
     }
 
 
 
-}
