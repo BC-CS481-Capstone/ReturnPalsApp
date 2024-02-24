8fb71714d57a5b491d49c7869196da2e4c240611
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-02-24 07:52:22 -0800

    add comments

--- app/src/main/java/com/example/returnpals/services/Backend.kt
+++ app/src/main/java/com/example/returnpals/services/Backend.kt
@@ -4,12 +4,13 @@ import android.util.Log
 import com.amplifyframework.AmplifyException
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
 import com.amplifyframework.core.Amplify
-
+/**Adds amplify backend on create code as tutorial examples provided.**/
 object Backend {
 
     private const val TAG = "Backend"
 
     fun initialize(applicationContext: Context) : Backend {
+        /**Adds amplify backend on create code as tutorial examples provided.**/
         try {
             Amplify.addPlugin(AWSCognitoAuthPlugin())
             Amplify.configure(applicationContext)
