3afaf0e85ce2cd7289324799328a596b9af19864
Gustavo-Gon <gon.gustavo@outlook.com>
2024-02-27 16:29:29 -0800

    More code testing added

--- app/src/androidTest/kotlin/com/example/returnpals/composetools/NavigationTest.kt
+++ app/src/androidTest/kotlin/com/example/returnpals/composetools/NavigationTest.kt
@@ -30,23 +30,42 @@ class NavigationTest {
         }
     }
 
-    // Unit test
     @Test
     fun appNavHost_verifyStartDestination() {
         assertEquals(MenuRoutes.Home, navController.currentDestination?.route)
     }
 
     @Test
-    fun navigateToProfile_verifyNavigation() {
+    fun navigateToHome_verifyNavigation() {
         // Simulate navigation action
         rule.runOnUiThread {
-            navController.navigate(MenuRoutes.Profile)
+            navController.navigate(MenuRoutes.Home)
         }
 
-        // Verify navigation to Profile
-        assertEquals(MenuRoutes.Profile, navController.currentDestination?.route)
+        // Verify navigation to Home
+        assertEquals(MenuRoutes.Home, navController.currentDestination?.route)
     }
 
+    @Test
+    fun navigateToAbout_verifyNavigation() {
+
+        rule.runOnUiThread {
+            navController.navigate(MenuRoutes.About)
+        }
+
+        assertEquals(MenuRoutes.About, navController.currentDestination?.route)
+    }
+
+    @Test
+    fun navigateToPricing_verifyNavigation() {
+
+        rule.runOnUiThread {
+            navController.navigate(MenuRoutes.Pricing)
+        }
+
+
+        assertEquals(MenuRoutes.Pricing, navController.currentDestination?.route)
+    }
     @Test
     fun navigateToContact_verifyNavigation() {
 
@@ -57,6 +76,73 @@ class NavigationTest {
         assertEquals(MenuRoutes.Contact, navController.currentDestination?.route)
 
     }
+
+    @Test
+    fun navigateToSignIn_verifyNavigation() {
+
+        rule.runOnUiThread {
+            navController.navigate(MenuRoutes.SignIn)
+        }
+
+        assertEquals(MenuRoutes.SignIn, navController.currentDestination?.route)
+
+    }
+
+    @Test
+    fun navigateToFAQ_verifyNavigation() {
+
+        rule.runOnUiThread {
+            navController.navigate(MenuRoutes.FAQ)
+        }
+
+        assertEquals(MenuRoutes.FAQ, navController.currentDestination?.route)
+
+    }
+
+    @Test
+    fun navigateToDashboard_verifyNavigation() {
+
+        rule.runOnUiThread {
+            navController.navigate(MenuRoutes.HomeDash)
+        }
+
+        assertEquals(MenuRoutes.HomeDash, navController.currentDestination?.route)
+
+    }
+
+    @Test
+    fun navigateToProfile_verifyNavigation() {
+
+        rule.runOnUiThread {
+            navController.navigate(MenuRoutes.Profile)
+        }
+
+        assertEquals(MenuRoutes.Profile, navController.currentDestination?.route)
+
+    }
+
+    @Test
+    fun navigateToSettings_verifyNavigation() {
+
+        rule.runOnUiThread {
+            navController.navigate(MenuRoutes.Settings)
+        }
+
+        assertEquals(MenuRoutes.Settings, navController.currentDestination?.route)
+
+    }
+
+    @Test
+    fun navigateToOrders_verifyNavigation() {
+
+        rule.runOnUiThread {
+            navController.navigate(MenuRoutes.Orders)
+        }
+
+        assertEquals(MenuRoutes.Orders, navController.currentDestination?.route)
+
+    }
+
 
     @Test
     fun navigateBackFromProfileToHome_verifyBackNavigation() {
