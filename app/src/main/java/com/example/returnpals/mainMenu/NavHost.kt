903d96cdd776a453f143d0fc9c06d96044eb93f1
andrew-pogue <andrewthomaspogue@gmail.com>
2024-02-29 02:39:00 -0800

    recreated schedule return view model and worked on navigation

--- app/src/main/java/com/example/returnpals/mainMenu/NavHost.kt
+++ app/src/main/java/com/example/returnpals/mainMenu/NavHost.kt
@@ -1,29 +1,40 @@
 package com.example.returnpals.mainMenu
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.createGraph
+import androidx.navigation.navigation
+import com.example.returnpals.composetools.ConfirmPickup
+import com.example.returnpals.composetools.PackagesUI
+import com.example.returnpals.composetools.PickupDateUI
+import com.example.returnpals.composetools.PickupMethodUI
+import com.example.returnpals.composetools.PricingUI
+import com.example.returnpals.composetools.ThankYou
 import com.example.returnpals.composetools.dashboard.HomeDash
 import com.example.returnpals.composetools.dashboard.Label
 import com.example.returnpals.composetools.dashboard.Orders
 import com.example.returnpals.composetools.dashboard.PickupDetails
-import com.example.returnpals.composetools.dashboard.PickupProcess
 import com.example.returnpals.composetools.dashboard.Profile
 import com.example.returnpals.composetools.dashboard.SelectAddress
 import com.example.returnpals.composetools.dashboard.Settings
+import com.example.returnpals.services.PickupInfo
+import com.example.returnpals.services.ScheduleReturnViewModel
 
-
-@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun AppNavigation(navController: NavController) {
 
-    // Create the navGraph
-    val navGraph = navController.createGraph(startDestination = MenuRoutes.Home) {
+    NavHost(
+        navController = navController as NavHostController,
+        startDestination = MenuRoutes.Home
+    ) {
+
         composable(MenuRoutes.Home) { Home(navController) }
         composable(MenuRoutes.About) { About(navController) }
         composable(MenuRoutes.Pricing) { Pricing(navController) }
@@ -36,15 +47,97 @@ fun AppNavigation(navController: NavController) {
         composable(MenuRoutes.Profile) { Profile(navController) }
         composable(MenuRoutes.Settings) { Settings(navController) }
         composable(MenuRoutes.Orders) { Orders(navController) }
-        composable(MenuRoutes.PickupProcess) { PickupProcess(navController) }
         composable(MenuRoutes.SelectAddress) { SelectAddress(navController) }
         composable(MenuRoutes.PickupDetails) { PickupDetails(navController) }
         composable(MenuRoutes.Label) { Label(navController) }
 
+        navigation(
+            startDestination = "select_date",
+            route = MenuRoutes.PickupProcess
+        ) {
+            composable("select_date") {
+                val viewModel = it.sharedViewModel<ScheduleReturnViewModel>(navController)
+                val pickup = viewModel.pickup.observeAsState(PickupInfo())
+                PickupDateUI(
+                    date = pickup.value.date,
+                    onChangeDate = viewModel::onChangeDate,
+                    isValidDate = viewModel::isValidDate,
+                    onClickNext = { navController.navigate("select_method") },
+                    onClickBack = { navController.navigate(MenuRoutes.HomeDash) },
+                )
+            }
+            composable("select_address") {
+//                val viewModel = it.sharedViewModel<ScheduleReturnViewModel>(navController)
+//                val pickup = viewModel.pickup.observeAsState(PickupInfo())
+            }
+            composable("select_method") {
+                val viewModel = it.sharedViewModel<ScheduleReturnViewModel>(navController)
+                val pickup = viewModel.pickup.observeAsState(PickupInfo())
+                PickupMethodUI(
+                    method = pickup.value.method,
+                    onChangeMethod = viewModel::onChangeMethod,
+                    onClickNext = { navController.navigate("select_pricing") },
+                    onClickBack = { navController.navigate("select_date") },
+                )
+            }
+            composable("select_pricing") {
+                val viewModel = it.sharedViewModel<ScheduleReturnViewModel>(navController)
+                val pickup = viewModel.pickup.observeAsState(PickupInfo())
+                PricingUI(
+                    plan = pickup.value.pricing,
+                    onChangePlan = viewModel::onChangePlan,
+                    onClickNext = { navController.navigate("add_labels") },
+                    onClickBack = { navController.navigate("select_method") },
+                )
+            }
+            composable("add_labels") {
+                val viewModel = it.sharedViewModel<ScheduleReturnViewModel>(navController)
+                val pickup = viewModel.pickup.observeAsState(PickupInfo())
+                PackagesUI(
+                    packages = pickup.value.packages.values.toList(),
+                    onAddLabel = viewModel::onAddLabel,
+                    onRemoveLabel = viewModel::onRemoveLabel,
+                    onClickNext = { navController.navigate("confirm") },
+                    onClickBack = { navController.navigate("select_pricing") },
+                )
+            }
+            composable("confirm") {
+                val viewModel = it.sharedViewModel<ScheduleReturnViewModel>(navController)
+                val pickup = viewModel.pickup.observeAsState(PickupInfo())
+                ConfirmPickup().drawConfirmPickup(
+                    typeOfPickup = pickup.value.method.toString(),
+//                    TODO: change type of pickupDate parameter to LocalDate
+//                       OR change type of PackageInfo.date to Calendar
+//                    TODO: show pricing only if BRONZE plan is selected
+                    pickUpAddress = pickup.value.address!!,
+                    nextButton = { navController.navigate("thanks") },
+                    backButton = { navController.navigate("add_labels") },
+                    promoButton = {}
+                )
+            }
+            composable("thanks") {
+                val viewModel = it.sharedViewModel<ScheduleReturnViewModel>(navController)
+//                val pickup = viewModel.pickup.observeAsState(PickupInfo())
+                ThankYou().drawThankYouUI(
+                    dashBoardButton = {
+                        viewModel.onSubmit()
+                        navController.navigate(MenuRoutes.HomeDash)
+                    }
+                )
+            }
+        }
 
-        // Add more destinations as needed
     }
 
-    // Use the navGraph in NavHost
-    NavHost(navController = navController as NavHostController, graph = navGraph)
+}
+
+
+// magic https://youtu.be/FIEnIBq7Ups?si=O2ePHcmj0VsmQ7R-
+@Composable
+inline fun <reified T:ViewModel> NavBackStackEntry.sharedViewModel(navController: NavController): T {
+    val parentRoute = destination.parent?.route ?: return viewModel()
+    val parentEntry = remember(this) {
+        navController.getBackStackEntry(parentRoute)
+    }
+    return viewModel(parentEntry)
 }
--- app/src/main/java/com/example/returnpals/services/ScheduleReturn.kt
+++ app/src/main/java/com/example/returnpals/services/ScheduleReturn.kt
@@ -1,14 +1,20 @@
-package com.example.returnpals
+package com.example.returnpals.services
 
 import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
 import com.example.returnpals.composetools.PickupMethodUI
 import com.example.returnpals.composetools.ConfirmPickup
 import com.example.returnpals.composetools.PackagesUI
@@ -17,6 +23,7 @@ import com.example.returnpals.composetools.PricingUI
 import com.example.returnpals.composetools.ThankYou
 import com.example.returnpals.composetools.goto
 import com.example.returnpals.mainMenu.MenuRoutes
+import java.time.LocalDate
 
 /**
  * https://developer.android.com/jetpack/compose/state
@@ -31,136 +38,44 @@ import com.example.returnpals.mainMenu.MenuRoutes
  * https://developer.android.com/topic/libraries/architecture/viewmodel/viewmodel-apis
  */
 
-/////////////////////////////////////////////////////////////////////////////
-// PUBLIC API
-////////////////////
+class ScheduleReturnViewModel(
+    pickup: PickupInfo = PickupInfo(),
+    val navController: NavController? = null,
+    private val minDate: LocalDate = LocalDate.now().minusDays(1),
+    private val maxDate: LocalDate = LocalDate.now().plusYears(1),
+) : PickupViewModel(pickup) {
 
-enum class ScheduleReturnStep {
-    SelectDate,
-    SelectAddress,
-    SelectMethod,
-    SelectPricing,
-    AddLabels,
-    Confirmation,
-    Thanks;
-
-    operator fun inc(): ScheduleReturnStep {
-//        if (this == Thanks) throw overflow error
-        return ScheduleReturnStep.values()[this.ordinal+1]
+    fun isValidDate(value: LocalDate): Boolean {
+        return run {
+            value > minDate && value < maxDate
+        }.also {
+            Log.println(Log.INFO, "ScheduleReturnViewModel::isValidDate", it.toString())
+        }
     }
 
-    operator fun dec(): ScheduleReturnStep {
-//        if (this == Thanks) throw overflow error
-        return ScheduleReturnStep.values()[this.ordinal-1]
+    fun onSubmit() {
+        // TODO: send pickup info to repository
+        Log.println(Log.INFO, "ScheduleReturnViewModel::onSubmit", pickup.value.toString())
     }
-}
 
-@Composable
-fun DemoScheduleReturn(
-    pickupVM: PickupViewModel
-) {
-    val controller = rememberNavController()
-    ScheduleReturn(
-        navController = controller,
-        pickupVM = pickupVM
-    )
-}
-
-@Composable
-fun ScheduleReturn(
-    navController: NavController,
-    pickupVM: PickupViewModel,
-) {
-    val step = remember { mutableStateOf(ScheduleReturnStep.values()[0]) }
-
-    ScheduleReturnContent(step.value, pickupVM,
-        onClickNext = { step.value++; Log.println(Log.INFO, "onClickNext", step.value.toString()) },
-        onClickBack = { step.value--; Log.println(Log.INFO, "onClickBack", step.value.toString()) },
-        onSubmit = { goto(navController, MenuRoutes.HomeDash) }
-    )
-
-}
-
-@Composable
-fun DemoSelectDate() {
-    val pickupVM = PickupViewModel(PickupInfo())
-    val pickup = pickupVM.data.observeAsState(PickupInfo())
-    PickupDateUI(
-        date = pickup.value.date,
-        onChangeDate = pickupVM::onChangeDate,
-        onClickNext = { Log.println(Log.INFO, "onClickNext", pickup.value.date.toString()) },
-        onClickBack = { Log.println(Log.INFO, "onClickBack", pickupVM.data.value?.date.toString()) }
-    )
-}
-
-/////////////////////////////////////////////////////////////////////////////
-// PRIVATE API
-////////////////////
-
-@Preview
-@Composable
-private fun ScheduleReturnPreview() {
-    ScheduleReturnContent(
-        ScheduleReturnStep.AddLabels,
-        PickupViewModel(PickupInfo()),
-        onClickNext = {},
-        onClickBack = {},
-        onSubmit = {}
-    )
-}
-
-@Composable
-private fun ScheduleReturnContent(
-    step: ScheduleReturnStep,
-    pickupVM: PickupViewModel,
-    onClickNext: () -> Unit,
-    onClickBack: () -> Unit,
-    onSubmit: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    val pickup = pickupVM.data.observeAsState(PickupInfo())
-    val confirm = ConfirmPickup()
-
-    when (step) {
-        ScheduleReturnStep.SelectDate -> PickupDateUI(
-            date = pickup.value.date,
-            onChangeDate = pickupVM::onChangeDate,
-            isValidDate = pickupVM::isValidDate,
-            onClickNext = { onClickNext(); onClickNext() },
-            onClickBack = onClickBack,
-            modifier = modifier)
-        ScheduleReturnStep.SelectAddress -> onClickNext()
-//            SelectAddressContent(navController = navController)
-        ScheduleReturnStep.SelectMethod -> PickupMethodUI(
-            method = pickup.value.method,
-            onChangeMethod = pickupVM::onChangeMethod,
-            onClickNext = onClickNext,
-            onClickBack = { onClickBack(); onClickBack() },
-            modifier = modifier)
-        ScheduleReturnStep.SelectPricing -> PricingUI(
-            plan = pickup.value.pricing,
-            onChangePlan = pickupVM::onChangePlan,
-            onClickNext = onClickNext,
-            onClickBack = onClickBack,
-            modifier = modifier)
-        ScheduleReturnStep.AddLabels -> PackagesUI(
-            packages = pickup.value.packages.values.toList(),
-            onAddLabel = pickupVM::onAddLabel,
-            onRemoveLabel = pickupVM::onRemoveLabel,
-            onClickNext = onClickNext,
-            onClickBack = onClickBack)
-        ScheduleReturnStep.Confirmation -> confirm.drawConfirmPickup(
-            typeOfPickup = pickup.value.method.toString(),
-//            TODO: change type of pickupDate parameter to LocalDate
-//               OR change type of PackageInfo.date to Calendar
-//            TODO: show pricing only if BRONZE plan is selected
-            pickUpAddress = pickup.value.address!!,
-            nextButton = onClickNext,
-            backButton = onClickBack,
-            promoButton = {})
-        ScheduleReturnStep.Thanks -> ThankYou().drawThankYouUI(
-            dashBoardButton = onSubmit
-        )
+    fun onCancel() {
+        if (navController != null)
+            goto(navController, MenuRoutes.Home)
+        Log.println(Log.INFO, "ScheduleReturnViewModel::onCancel", "")
     }
+
+//    companion object {
+//
+//        // This determines the default value for the view-model that gets instantiated in navigation.
+//        // https://developer.android.com/topic/libraries/architecture/viewmodel/viewmodel-factories
+//        val Factory: ViewModelProvider.Factory = viewModelFactory {
+//            initializer {
+//                ScheduleReturnViewModel(
+//                    navController = ,
+//                )
+//            }
+//        }
+//
+//    }
 
 }
