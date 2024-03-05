636cd7f5dcf16c48af3c972e8fb8c58d38b89931
Gustavo-Gon <gon.gustavo@outlook.com>
2024-03-05 11:38:52 -0800

    Registration form added

--- app/src/main/java/com/example/returnpals/services/AmplifyOperations.kt
+++ app/src/main/java/com/example/returnpals/services/AmplifyOperations.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.amplifyframework.core.Amplify
 import com.amplifyframework.datastore.generated.model.MailingList
 import com.amplifyframework.api.graphql.model.ModelMutation
+import com.amplifyframework.datastore.generated.model.UsersMongoDb
 
 object AmplifyOperations {
 
@@ -20,4 +21,23 @@ object AmplifyOperations {
             { error -> onError(error) }
         )
     }
+
+    fun sendRegistrationData(model: UsersMongoDb, onSuccess: (String) -> Unit, onError: (Throwable) -> Unit) {
+        Amplify.API.mutate(
+            ModelMutation.create(model),
+            { response ->
+                Log.d("AmplifyOperations", "Response: $response")
+                if(response.hasErrors()) {
+                    onError(Exception("Error submitting Registration data: ${response.errors.first().message}"))
+                } else {
+                    onSuccess(response.data.id)
+                }
+            },
+            { error ->
+                Log.e("AmplifyOperations", "Error submitting Registration data", error)
+                onError(error)
+            }
+        )
+    }
+
 }
