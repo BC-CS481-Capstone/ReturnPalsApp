6c168bddff51f33fddcedc5e2ab881af501d72b8
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-05-01 08:23:18 -0700

    save test

--- app/src/main/java/com/example/returnpals/MainActivity.kt
+++ app/src/main/java/com/example/returnpals/MainActivity.kt
@@ -9,11 +9,29 @@ import androidx.navigation.compose.rememberNavController
 import com.example.compose.ReturnPalTheme
 import com.example.returnpals.mainMenu.AppNavigation
 import com.example.returnpals.services.Backend
+import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.net.HttpURLConnection
+import java.net.URL
 
 var email : String = ""
 
+
+
 class MainActivity : ComponentActivity() {
+    private var client =  OkHttpClient()
     override fun onCreate(savedInstanceState: Bundle?) {
+        var url =  URL("http://localhost:4200/")
+        with (url.openConnection() as HttpURLConnection) {
+            requestMethod = "POST"
+        }
+        var requestbody = MultipartBody.Builder().setType(MultipartBody.FORM).addFormDataPart("","")
+        var request = Request.Builder().url("http://localhost:4200/").build()
+        var response = client.newCall(request).execute()
+        response.isSuccessful
+
+
 
         Backend.accessEmail()
         super.onCreate(savedInstanceState)
