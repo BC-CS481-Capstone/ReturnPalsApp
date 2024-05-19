ba15033f053d6078d09b997de93621f1ce0c3972
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-05-19 15:15:44 -0700

    Layered UI with view model in MainMenu Screen section

---
+++ app/src/main/java/com/example/returnpals/composetools/mainMenu/MainMenuScreen.kt
@@ -0,0 +1,89 @@
+package com.example.returnpals.composetools.mainMenu
+
+import MainMenuScaffold
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import com.example.returnpals.data.MainMenuScreenRepository
+import com.example.returnpals.navigation.MenuRoutes
+
+
+/**
+ * This file is a top level
+ *
+ *
+ * */
+
+
+
+//This file create the navigation for the main menu.
+//Then navigate to either the dashboard or sign in.
+
+@Composable
+fun MainMenuScreen(mainMenuScreenViewModel: MainMenuScreenViewModel) {
+    val selectedItem by mainMenuScreenViewModel.navRoute.observeAsState()
+    MainMenuScaffold(navigateRoute = mainMenuScreenViewModel::selectedNavRoute) {
+        when(selectedItem){
+            "sign in"->mainMenuScreenViewModel.onSignInOrScheduleButton()
+            "home"->HomeContent(){mainMenuScreenViewModel.selectedNavRoute("sign in") }
+            "about"->AboutContent()
+            "pricing"-> PricingContent()
+            "contact"-> ContactContent()
+            "faq"-> FAQContent()
+        }
+    }
+}
+
+
+class MainMenuScreenViewModel(
+    private val mainMenuScreenRepository: MainMenuScreenRepository,
+    private val navController: NavController
+): ViewModel() {
+    /** Following the tutorial information from https://developer.android.com/topic/libraries/architecture/viewmodel
+     * This is a view model for the MainMenuScreen.
+     * This holds the state if the user is signed in or not.
+     * */
+    private val _isSignedIn = mutableStateOf<Boolean?>(null)
+
+    private val _navRoute = MutableLiveData<String?>("home")
+    val navRoute: LiveData<String?> = _navRoute
+    suspend fun checkSignedIn(){
+        mainMenuScreenRepository.isSignedIn { isSignedIn ->
+            _isSignedIn.value = isSignedIn
+        }
+    }
+    fun selectedNavRoute(it:String) {
+        _navRoute.postValue(it)
+    }
+    @Composable
+    fun onSignInOrScheduleButton() {
+        var route = ""
+        if (_isSignedIn.value == true) {
+            route = MenuRoutes.HomeDash
+        } else if (_isSignedIn.value == false) {
+            route = MenuRoutes.SignIn
+        } else {
+            Log.e("MainMenuScreenViewModel","Unknown if user is signed in or not already. Suspect API call has not returned.")
+          return
+        }
+        navController.navigate(route) { // Clear all the back stack up to the start destination and save state
+            popUpTo(navController.graph.findStartDestination().id) {
+                saveState = true
+            }
+            // Avoid multiple copies of the same destination when reselecting the same item
+            launchSingleTop = true
+            // Restore state when navigating back to the composable
+            restoreState = true
+        }
+    }
+
+}
+
+
--- app/src/main/java/com/example/returnpals/navigation/MainMenu.kt
+++ app/src/main/java/com/example/returnpals/navigation/MainMenu.kt
@@ -1,65 +1,34 @@
 package com.example.returnpals.navigation
 
-import MainMenuScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import com.example.returnpals.composetools.mainMenu.AboutContent
-import com.example.returnpals.composetools.mainMenu.ContactContent
-import com.example.returnpals.composetools.mainMenu.FAQContent
-import com.example.returnpals.composetools.mainMenu.HomeContent
-import com.example.returnpals.composetools.mainMenu.PricingContent
-import com.example.returnpals.data.CognitoMainMenuRepository
+import com.example.returnpals.composetools.mainMenu.MainMenuScreen
+import com.example.returnpals.composetools.mainMenu.MainMenuScreenViewModel
+import com.example.returnpals.data.CognitoMainMenuScreenRepository
 
 //This file create the navigation for the main menu.
 //Then navigate to either the dashboard or sign in.
 
 @Composable
 fun MainMenu(navController: NavController) {
-    val context = LocalContext.current
-    var selectedItem by remember { mutableStateOf("Home") }
-    var isSignedIn by remember {mutableStateOf<Boolean?>(null)}
-    LaunchedEffect(context) {
-        CognitoMainMenuRepository().isSignedIn { isSignedIn = it }
-    }
-    if (selectedItem == "Sign In") {
-        if (isSignedIn == true) {
-            navController.navigate(MenuRoutes.HomeDash) { // Clear all the back stack up to the start destination and save state
-                popUpTo(navController.graph.findStartDestination().id) {
-                    saveState = true
-                }
-                // Avoid multiple copies of the same destination when reselecting the same item
-                launchSingleTop = true
-                // Restore state when navigating back to the composable
-                restoreState = true
-            }
-        } else if (isSignedIn == false) {
-            navController.navigate(MenuRoutes.SignIn) { // Clear all the back stack up to the start destination and save state
-                popUpTo(navController.graph.findStartDestination().id) {
-                    saveState = true
-                }
-                // Avoid multiple copies of the same destination when reselecting the same item
-                launchSingleTop = true
-                // Restore state when navigating back to the composable
-                restoreState = true
-            }
-        }
 
+    val context = LocalContext.current
+    //Create view model
+    val viewModel = MainMenuScreenViewModel(
+        CognitoMainMenuScreenRepository(),
+        navController = navController
+    )
+    LaunchedEffect(context) {
+        //Launch API call to talk to server
+        viewModel.checkSignedIn()
     }
-    MainMenuScaffold(navController = navController,{selectedItem = it }) {
-        when(selectedItem){
-            "Home"->HomeContent(){selectedItem = "Sign In" }
-            "About"->AboutContent()
-            "Pricing"-> PricingContent()
-            "Contact"-> ContactContent()
-            "FAQ"-> FAQContent()
-        }
-    }
+    MainMenuScreen(
+        mainMenuScreenViewModel = viewModel,
+        )
 }
+
+
+
 
