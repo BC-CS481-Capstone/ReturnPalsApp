6ac3ae3b259dc1715100b7cfe16dc70cf3b640d5
andrew-pogue <andrewthomaspogue@gmail.com>
2024-05-23 16:15:23 -0700

    incomplete work on guest address screen

--- app/src/main/java/com/example/returnpals/composetools/pickup/SelectAddress.kt
+++ app/src/main/java/com/example/returnpals/composetools/pickup/SelectAddress.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -34,12 +35,20 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.example.compose.ReturnPalTheme
+import com.example.returnpals.AddressInfo
 import com.example.returnpals.composetools.ScheduleReturnScaffold
+import java.util.Locale
 
 // A view model is necessary here to remember which address option was selected across screens.
 
-@Preview
+fun getCountries(): List<String> {
+    return Locale.getISOCountries().map { countryCode ->
+        Locale("", countryCode).displayCountry
+    }
+}
+
 @Composable
 fun SelectAddressScreen(
     addresses: List<SettingsViewModel.SimpleAddress> = listOf(),
@@ -50,7 +59,6 @@ fun SelectAddressScreen(
     onAddAddress: (String) -> Unit = {},
     isGuest: Boolean = false,
 ) {
-
     if (addresses.isEmpty()) {
         Log.d("SelectAddressScreen", "No addresses available")
     } else {
@@ -63,18 +71,24 @@ fun SelectAddressScreen(
         onClickNext = onClickNext,
         enabledNext = selectedAddressId != null,
     ) { padding ->
-        SelectAddressContent(
-            selected = selectedAddressId,
-            userAddresses = addresses,
-            onSelectAddress = onSelectAddress,
-            onAddAddress = onAddAddress,
-            onClickNext = onClickNext,
-            onClickBack = onClickBack,
-            isGuest = false,
-            modifier = Modifier
-                .padding(padding)
-                .padding(20.dp)
-        )
+        if (isGuest)
+            SelectAddressGuestContent(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+                    .padding(20.dp)
+            )
+        else
+            SelectAddressContent(
+                selected = selectedAddressId,
+                userAddresses = addresses,
+                onSelectAddress = onSelectAddress,
+                onAddAddress = onAddAddress,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+                    .padding(20.dp)
+            )
     }
 }
 
@@ -86,18 +100,18 @@ fun SelectAddressContent(
     userAddresses: List<SettingsViewModel.SimpleAddress>,
     onSelectAddress: (Int) -> Unit,
     onAddAddress: (String) -> Unit,
-    onClickNext: () -> Unit,
-    onClickBack: () -> Unit,
-    isGuest: Boolean,
     modifier: Modifier = Modifier
 ) {
     Column(modifier = modifier) {
-        Spacer(modifier = Modifier.height(16.dp))
-        AddressHeader()
-
-        Spacer(modifier = Modifier.height(16.dp))
-        AddressInfo()
-
+    Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Your Addresses:",
+            style = TextStyle(
+                color = Color.Black,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Normal,
+            )
+        )
         Spacer(modifier = Modifier.height(16.dp))
         LazyColumn {
             items(userAddresses) { address ->
@@ -109,11 +123,42 @@ fun SelectAddressContent(
                 )
             }
         }
-
         AddAddressField(onAddAddress)
     }
 }
 
+@Composable
+fun SelectAddressGuestContent(
+    modifier: Modifier = Modifier
+) {
+    var address by remember { mutableStateOf(AddressInfo()) }
+    Column(modifier) {
+        AddressHeader()
+        AddressFields(address, modifier.padding(20.dp)) { address = it }
+    }
+}
+
+@Preview
+@Composable
+private fun SelectAddressPreview() {
+    ReturnPalTheme {
+        SelectAddressScreen(
+            addresses = listOf(
+                SettingsViewModel.SimpleAddress("1", "one"),
+                SettingsViewModel.SimpleAddress("2", "two"),
+                SettingsViewModel.SimpleAddress("3", "three"),
+                SettingsViewModel.SimpleAddress("4", "four"),
+                SettingsViewModel.SimpleAddress("5", "five"),
+                SettingsViewModel.SimpleAddress("6", "six"),
+                SettingsViewModel.SimpleAddress("7", "seven"),
+                SettingsViewModel.SimpleAddress("8", "eight"),
+                SettingsViewModel.SimpleAddress("9", "nine"),
+                SettingsViewModel.SimpleAddress("10", "ten"),
+            ),
+            isGuest = true
+        )
+    }
+}
 
 @Composable
 private fun AddressHeader() {
@@ -123,9 +168,8 @@ private fun AddressHeader() {
             style = TextStyle(
                 color = Color.Black,
                 fontSize = 32.sp,
-                fontWeight = FontWeight.Normal,
-
-                )
+                fontWeight = FontWeight.Normal
+            )
         )
 
         Spacer(modifier = Modifier.height(32.dp))
@@ -139,31 +183,14 @@ private fun AddressHeader() {
             )
         )
     }
-    Spacer(modifier = Modifier.height(88.dp))
 }
 
 @Composable
-private fun AddressInfo(){
-    Column {
-        Text(
-            text = "Your Addresses:",
-            style = TextStyle(
-                color = Color.Black,
-                fontSize = 14.sp,
-                fontWeight = FontWeight.Normal,
-            )
-        )
-    }
-}
-
-
-
-@Composable
-fun AddAddressField(onAddAddress: (String) -> Unit) {
+private fun AddAddressField(
+    onAddAddress: (String) -> Unit,
+    isGuest: Boolean = false,
+) {
     var address by remember { mutableStateOf("") }
-
-
-
     Column(
         modifier = Modifier
             .padding(16.dp)
@@ -172,29 +199,30 @@ fun AddAddressField(onAddAddress: (String) -> Unit) {
         TextField(
             value = address,
             onValueChange = { address = it },
-            label = { Text("Enter new address") },
+            label = { Text(if (isGuest) "Enter your address" else "Enter new address") },
             modifier = Modifier.fillMaxWidth()
         )
 
+        if (!isGuest) {
+            Button(
+                onClick = {
+                    if (address.isNotBlank()) {
+                        onAddAddress(address)
+                        address = ""
 
-        Button(
-            onClick = {
-                if (address.isNotBlank()) {
-                    onAddAddress(address)
-                    address = ""
-
-                }
-            },
-            shape = RoundedCornerShape(8.dp),
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = Color(0xFF008BE7),
-                contentColor = Color.White
-            ),
-            modifier = Modifier
-                .padding(top = 8.dp)
-                .align(Alignment.End)
-        ) {
-            Text("Add Address")
+                    }
+                },
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color(0xFF008BE7),
+                    contentColor = Color.White
+                ),
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .align(Alignment.End)
+            ) {
+                Text("Add Address")
+            }
         }
     }
 }
@@ -235,5 +263,110 @@ private fun AddressItem(
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun AddressDialogue(
+    modifier: Modifier = Modifier,
+    init: AddressInfo = AddressInfo(),
+    title: String = "Add a new address",
+    buttonText: String = "Add Address",
+    onCancel: () -> Unit = {},
+    onConfirm: (AddressInfo) -> Unit = {}
+) {
+    var address by remember { mutableStateOf(init) }
+    Dialog(onDismissRequest = onCancel) {
+        Column(modifier) {
+            Text(
+                text = title,
+                color = ReturnPalTheme.colorScheme.secondary,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(30.dp)
+            )
+            AddressFields(address) { address = it }
+            Row {
+                Spacer(Modifier.weight(1f))
+                Button(
+                    onClick = { onConfirm(address) },
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color(0xFF008BE7),
+                        contentColor = Color.White
+                    ),
+                    modifier = Modifier
+                        .padding(top = 20.dp)
+                ) {
+                    Text(buttonText)
+                }
+            }
+        }
+    }
+}
 
+@Composable
+private fun AddressFields(
+    address: AddressInfo,
+    modifier: Modifier = Modifier,
+    onChange: (AddressInfo) -> Unit = {},
+) {
+    val spacing = 20.dp
+    LazyColumn(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        item {
+            TextField(
+                value = address.country,
+                onValueChange = { onChange(address.copy(country=it)) },
+                label = { Text(text = "Country") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(Modifier.height(spacing))
+        }
+        item {
+            TextField(
+                value = address.street,
+                onValueChange = { onChange(address.copy(street = it)) },
+                label = { Text(text = "Street Address") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(Modifier.height(spacing))
+        }
+        item {
+            TextField(
+                value = address.unit,
+                onValueChange = { onChange(address.copy(unit = it)) },
+                label = { Text(text = "Apt, Suite, Building, Floor, ect") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(Modifier.height(spacing))
+        }
+        item {
+            TextField(
+                value = address.city,
+                onValueChange = { onChange(address.copy(city = it)) },
+                label = { Text(text = "City") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(Modifier.height(spacing))
+        }
+        item {
+            TextField(
+                value = address.state,
+                onValueChange = { onChange(address.copy(state = it)) },
+                label = { Text(text = "Province or State") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(Modifier.height(spacing))
+        }
+        item {
+            TextField(
+                value = address.zipcode,
+                onValueChange = { onChange(address.copy(zipcode = it)) },
+                label = { Text(text = "ZIP Code") },
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
 
