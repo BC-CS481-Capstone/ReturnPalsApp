707c27bdac67d1cac52c02bbcb2748175e026481
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-05-23 09:35:17 -0700

    Removed Unused objects

--- app/src/main/java/com/example/returnpals/viewmodel/backend/DatastoreRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.returnpals.services.backend
-
-/**
- * Encapsulates create, read, update, and delete (CRUD) operations for a specified data model using Datastore API.
- */
-open class DatastoreRepository {
-}
