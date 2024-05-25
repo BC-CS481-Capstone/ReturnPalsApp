00880663624760f289fc8a0378d78359614a01ed
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-05-25 14:43:24 -0700

    Wrote fun description

--- app/src/main/java/com/example/returnpals/dataRepository/RegisterRepository.kt
+++ app/src/main/java/com/example/returnpals/dataRepository/RegisterRepository.kt
@@ -3,8 +3,11 @@ package com.example.returnpals.dataRepository
 import com.example.returnpals.viewmodel.RegisterUserInfo
 
 interface RegisterRepository {
-
-    fun registerUser(registerUserInfo: RegisterUserInfo, errorMessage:(String?)->Unit)
+    //>registerUser takes the view models state of the registerScreen and a callback
+    // @result #Boolean = true when operation is a success
+    //         #Boolean = false when failed operation
+    //         #String = fail message
+    fun registerUser(registerUserInfo: RegisterUserInfo, result:(Boolean,String?)->Unit)
 
 }
 
