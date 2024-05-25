161348c93733b8e9474ea4f5f888b1a3dc0f69ee
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-05-25 12:53:13 -0700

    refactor names and move Register() to new file for navigation

--- app/src/main/java/com/example/returnpals/navigation/Register.kt
+++ app/src/main/java/com/example/returnpals/navigation/Register.kt
@@ -1,24 +1,30 @@
 package com.example.returnpals.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.navigation.NavController
+import com.example.returnpals.composetools.login.ConfirmEmailScreen
 import com.example.returnpals.composetools.login.RegisterScreen
-import com.example.returnpals.dataRepository.RegisterRepository
-import com.example.returnpals.viewmodel.RegisterUserInfo
+import com.example.returnpals.dataRepository.RegisterRepositoryAmplify
+import com.example.returnpals.services.ConfirmEmailViewModel
 import com.example.returnpals.viewmodel.RegisterViewModel
-
+val registerRepository = RegisterRepositoryAmplify()
+val registerVM = RegisterViewModel(registerRepository)
 @Composable
-fun Register(navController:NavController) {
-    val registerViewModel = RegisterViewModel(navController = navController, registerRepository = TestRepositoryforRegister() )
-    RegisterScreen(viewModel())
-}
+fun Register(navController: NavController) {
 
-class TestRepositoryforRegister: RegisterRepository {
-
-    override fun registerUser(registerUserInfo: RegisterUserInfo, errorMessage: (String?) -> Unit) {
-        TODO("Not yet implemented")
+    val confirmEmailVM = ConfirmEmailViewModel()
+    /** Navigation Logic HERE */
+    //Condition variables from view models
+    val confirmSuccessful by confirmEmailVM.confirmSuccessful.observeAsState()
+    if (confirmSuccessful == true) navController.goto(MenuRoutes.SignIn)
+    /** Navigation Logic STOP */
+    val submissionSuccessful by registerVM.submissionSuccessful.observeAsState()
+    val registerUIState by registerVM.uiState.collectAsState()
+    if (submissionSuccessful == true) {
+        ConfirmEmailScreen(navController, ConfirmEmailViewModel(registerUIState.email))
     }
-
-
+    RegisterScreen(registerVM)
 }
