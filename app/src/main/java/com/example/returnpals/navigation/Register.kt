9bbc605947f93f52e467fc77d6772ebdada49e64
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-05-25 21:18:18 -0700

    moved logic to view model

--- app/src/main/java/com/example/returnpals/navigation/Register.kt
+++ app/src/main/java/com/example/returnpals/navigation/Register.kt
@@ -1,30 +1,10 @@
 package com.example.returnpals.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.navigation.NavController
-import com.example.returnpals.composetools.login.ConfirmEmailScreen
 import com.example.returnpals.composetools.login.RegisterScreen
-import com.example.returnpals.dataRepository.RegisterRepositoryAmplify
-import com.example.returnpals.services.ConfirmEmailViewModel
 import com.example.returnpals.viewmodel.RegisterViewModel
-val registerRepository = RegisterRepositoryAmplify()
-val registerVM = RegisterViewModel(registerRepository)
-@Composable
-fun Register(navController: NavController) {
 
-    val confirmEmailVM = ConfirmEmailViewModel()
-    /** Navigation Logic HERE */
-    //Condition variables from view models
-    val confirmSuccessful by confirmEmailVM.confirmSuccessful.observeAsState()
-    if (confirmSuccessful == true) navController.goto(MenuRoutes.SignIn)
-    /** Navigation Logic STOP */
-    val submissionSuccessful by registerVM.submissionSuccessful.observeAsState()
-    val registerUIState by registerVM.uiState.collectAsState()
-    if (submissionSuccessful == true) {
-        ConfirmEmailScreen(navController, ConfirmEmailViewModel(registerUIState.email))
-    }
+@Composable
+fun Register(registerVM: RegisterViewModel) {
     RegisterScreen(registerVM)
 }
