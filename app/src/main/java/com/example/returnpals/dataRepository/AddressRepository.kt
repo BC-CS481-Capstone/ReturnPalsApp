1fd0ce217578407ee3515be205d259121be83c62
David Caldwell <david.caldwell@bellevuecollege.edu>
2024-05-26 00:35:16 -0700

    Testing with address problems

--- app/src/main/java/com/example/returnpals/dataRepository/AddressRepository.kt
+++ app/src/main/java/com/example/returnpals/dataRepository/AddressRepository.kt
@@ -2,7 +2,7 @@ package com.example.returnpals.dataRepository
 
 interface AddressRepository {
 
-    fun addNewAddress(address:String,result:(Boolean)->Unit)
+    fun addNewAddress(address: String, name: String, result: (Boolean) -> Unit)
 
     fun deleteAddress(address:String,result:(Boolean)->Unit)
 
